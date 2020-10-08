b7bcf14c46809892a0e51baea3e7be67a1bb6e5f
Suzanne Cabral <suzannecabral@live.com>
2020-10-07 19:02:47 -0700

    Mapped over fake data

--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -1,10 +1,41 @@
 import React from "react";
+// import shortid from "shortid";
+
+const tasks = [
+    {
+        taskName:"Write Lists",
+        taskDone:false,
+        taskId:"w8NUAz9AZq",
+    },
+    {
+        taskName:"Bake Cookies",
+        taskDone:false,
+        taskId:"r7bEZzB3Lr",
+    },
+    {
+        taskName:"Carve a Pumpkin",
+        taskDone:false,
+        taskId:"nDwfYvPTP1",
+    },
+];
+
+//generated from shortid:
+//n46IX7mLB
+//kUMVAQpSgP
+//0y2jKQdRRd
+//XbIIKrNQKc
 
 const TodoList = (props) => {
 
     return(
         <ul>
-            <li>test</li>
+            {
+            tasks.map((item)=>{
+                return (
+                    <li key={item.taskId}>{item.taskName}</li>
+                );
+             })
+            }
         </ul>
 
     );
